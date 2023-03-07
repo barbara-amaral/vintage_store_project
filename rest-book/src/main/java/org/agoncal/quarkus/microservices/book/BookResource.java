c610e51603b411176fd21260780e852febd729a8
Bárbara Amaral <barbara.souza.amaral@gmail.com>
2023-03-06 23:51:17 -0300

    Creating and testing the book microservice

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,39 @@
 package org.agoncal.quarkus.microservices.book;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Path("api/books")
+@Tag(name = "Book REST endpoint")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Creates a book",
+            description = "Creates a book with an ISBN number"
+    )
+    public Response createABookResponse(@FormParam("title") String title, @FormParam("author") String author, @FormParam("year") int yearOfPublication, @FormParam("genre") String genre) {
+        Book book = new Book();
+        book.isbn13 = "13-Will get from Numbers microservice";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        logger.info("Book created: " + book);
+
+        return Response.status(201).entity(book).build();
     }
 }
--- rest-book/src/test/java/org/agoncal/quarkus/microservices/book/BookResourceTest.java
+++ rest-book/src/test/java/org/agoncal/quarkus/microservices/book/BookResourceTest.java
@@ -5,17 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldCreateABook() {
         given()
-          .when().get("/api/books")
+                .formParam("title", "Quarkus")
+                .formParam("author", "Antonio")
+                .formParam("year", 2021)
+                .formParam("genre", "IT")
+          .when().post("/api/books")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .statusCode(201)
+             .body("isbn_13", startsWith("13-"))
+                .body("title", startsWith("Quarkus"))
+                .body("author", startsWith("Antonio"))
+                .body("year_of_publication", is(2021))
+                .body("genre", is("IT"))
+                .body("creation_date", startsWith("20"));
     }
 
 }
